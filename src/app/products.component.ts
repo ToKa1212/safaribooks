24735744128fa9062f2c181fd2cbdae1c5d3e78d
Kačička Tomáš <kacicka@cngroup.dk>
2019-03-23 17:38:07 +0100

    product listing + removing option using filter function

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -6,5 +6,21 @@ import {Component} from '@angular/core';
   templateUrl: './products.component.html'
 })
 export class ProductsComponent {
+  productName = '';
+  isDisabled = true;
+  products = ['gtx 1070', 'rtx 280 ti']
+
+  constructor() {
+    setTimeout(() =>
+      this.isDisabled = false, 3000);
+  }
+
+  onAddProduct() {
+    this.products.push(this.productName);
+  }
+
+  onRemoveProduct(productName: string) {
+    this.products = this.products.filter(p => p !== productName);
+  }
 
 }
